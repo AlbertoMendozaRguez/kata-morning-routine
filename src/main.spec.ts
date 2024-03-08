2a494714750cea435cacfe0dfb77cf55da32057a
Fernando Sanchez <fernandosoto0643@gmail.com>
2024-03-08 15:41:22 +0000

    test

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from "vitest"
+import { MorningRoutine } from "./main.js"
 
 describe("Default test", () => {
   it("display Do exercise between 06:00 and 06:59", () => {
@@ -6,5 +7,12 @@ describe("Default test", () => {
     const result = morningRoutine.whatShouldIDoNow()
 
     expect(result).toEqual("Do exercise")
+  })
+
+  it("display Read and study between 07:00 and 07:59", () => {
+    const morningRoutine = new MorningRoutine()
+    const result = morningRoutine.whatShouldIDoNow()
+
+    expect(result).toEqual("Read and study")
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
 export function myFunction() {
   return true
 }
+
+export class MorningRoutine {
+  whatShouldIDoNow() {
+    return "Do exercise"
+  }
+}
