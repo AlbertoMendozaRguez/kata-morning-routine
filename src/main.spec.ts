2b22aee743b25272fbb52d3cb50dd6a21f22ce13
AlbertoMendoza-AcidTango <albertomendoza@acidtango.com>
2024-03-08 16:09:31 +0000

    test: display Do exercise and Take a shower between 06:45 to 06:59

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -19,7 +19,8 @@ describe("Default test", () => {
   })
 
   it("display Have breakfast between 08:00 and 08:59", () => {
-    const morningRoutine = new MorningRoutineImplementation(new Date())
+    const fakeDate = new Date().setHours(8)
+    const morningRoutine = new MorningRoutineImplementation(new Date(fakeDate))
     const result = morningRoutine.whatShouldIDoNow()
 
     expect(result).toEqual("Have breakfast")
@@ -30,5 +31,12 @@ describe("Default test", () => {
     const result = morningRoutine.whatShouldIDoNow()
 
     expect(result).toEqual("No activity")
+  })
+
+  it("display Do exercise and Take a shower between 06:45 to 06:59", () => {
+    const morningRoutine = new MorningRoutineImplementation(new Date())
+    const result = morningRoutine.whatShouldIDoNow()
+
+    expect(result).toEqual("Do exercise,Take a shower")
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,16 @@ export class MorningRoutineImplementation implements MorningRoutine {
   constructor(private readonly currentMoment: Date) {}
 
   whatShouldIDoNow() {
-    if (this.currentMoment.getHours() >= 6 && this.currentMoment.getHours() < 7) return "Do exercise"
+    const activities: string[] = []
 
-    if (this.currentMoment.getHours() >= 7 && this.currentMoment.getHours() < 8) return "Read and study"
+    if (this.currentMoment.getHours() >= 6 && this.currentMoment.getHours() < 7) activities.push("Do exercise")
 
-    return "Have breakfast"
+    if (this.currentMoment.getHours() >= 7 && this.currentMoment.getHours() < 8) activities.push("Read and study")
+
+    if (this.currentMoment.getHours() >= 8 && this.currentMoment.getHours() < 9) activities.push("Have breakfast")
+
+    if (!activities.length) activities.push("No activity")
+
+    return activities.join(",")
   }
 }
