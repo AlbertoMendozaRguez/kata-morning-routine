0ac8f940c14f9569dee90223731810e3d711eaad
AlbertoMendoza-AcidTango <albertomendoza@acidtango.com>
2024-03-08 15:50:22 +0000

    test: display Have breakfast between 08:00 and 08:59

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,18 +1,26 @@
 import { describe, expect, it } from "vitest"
-import { MorningRoutine } from "./main.js"
+import { MorningRoutineImplementation } from "./main.js"
 
 describe("Default test", () => {
   it("display Do exercise between 06:00 and 06:59", () => {
-    const morningRoutine = new MorningRoutine()
+    const fakeDate = new Date().setHours(6)
+    const morningRoutine = new MorningRoutineImplementation(new Date(fakeDate))
     const result = morningRoutine.whatShouldIDoNow()
 
     expect(result).toEqual("Do exercise")
   })
 
   it("display Read and study between 07:00 and 07:59", () => {
-    const morningRoutine = new MorningRoutine()
+    const morningRoutine = new MorningRoutineImplementation(new Date())
     const result = morningRoutine.whatShouldIDoNow()
 
     expect(result).toEqual("Read and study")
+  })
+
+  it("display Have breakfast between 08:00 and 08:59", () => {
+    const morningRoutine = new MorningRoutineImplementation(new Date())
+    const result = morningRoutine.whatShouldIDoNow()
+
+    expect(result).toEqual("Have breakfast")
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-export function myFunction() {
-  return true
+interface MorningRoutine {
+  whatShouldIDoNow(): string
 }
 
-export class MorningRoutine {
+export class MorningRoutineImplementation implements MorningRoutine {
+  constructor(private readonly currentMoment: Date) {}
+
   whatShouldIDoNow() {
-    return "Do exercise"
+    if (this.currentMoment.getHours() >= 6 && this.currentMoment.getHours() < 7) return "Do exercise"
+
+    return "Read and study"
   }
 }
