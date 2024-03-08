3080d0ba9ec2e21eaed62a87445092490ff987a4
AlbertoMendoza-AcidTango <albertomendoza@acidtango.com>
2024-03-08 15:30:27 +0000

    test: should display Do exercise between 06:00 and 06:59

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,10 +1,10 @@
-import { describe, it, expect } from "vitest"
-import { myFunction } from "./main.js"
+import { describe, expect, it } from "vitest"
 
 describe("Default test", () => {
-  it("should work", () => {
-    const result = myFunction()
+  it("display Do exercise between 06:00 and 06:59", () => {
+    const morningRoutine = new MorningRoutine()
+    const result = morningRoutine.whatShouldIDoNow()
 
-    expect(result).toBe(true)
+    expect(result).toEqual("Do exercise")
   })
 })
